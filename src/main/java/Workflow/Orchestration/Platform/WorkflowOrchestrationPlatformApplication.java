2fa5c96a3b6b30f5d6021bc3ca047789428eeb02
Nithin L <nithin.l@nagra.com>
2026-02-22 12:23:31 +0530

    Project setup

--- src/main/java/Workflow/Orchestration/Platform/WorkflowOrchestrationPlatformApplication.java
+++ src/main/java/Workflow/Orchestration/Platform/WorkflowOrchestrationPlatformApplication.java
@@ -1,4 +1,4 @@
-package com.workflow.orchestration.platform;
+package Workflow.Orchestration.Platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
