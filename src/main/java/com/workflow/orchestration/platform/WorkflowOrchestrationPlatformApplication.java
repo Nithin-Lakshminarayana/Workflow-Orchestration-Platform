d3f996c38eb316308d25e1b4075f818eaf7f41e2
Nithin L <nithin.l@nagra.com>
2026-02-22 11:24:17 +0530

    Initial Commit

---
+++ src/main/java/com/workflow/orchestration/platform/WorkflowOrchestrationPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.workflow.orchestration.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkflowOrchestrationPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkflowOrchestrationPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workflow/orchestration/platform/WorkflowOrchestrationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.orchestration.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowOrchestrationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
